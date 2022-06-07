d9b618d42e197f4b9311657552f35d743a1eb0c4
Pc Bia - Substituto <renanamendola@hotmail.com>
2022-06-07 19:14:19 -0300

    componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { usuario } from '../model/usuario';
 import { usuarioLogin } from '../model/usuarioLogin';
 
@@ -23,6 +24,14 @@ cadastrar(usuario: usuario): Observable<usuario>{
   return this.http.post<usuario>('https://bprenan.herokuapp.com/usuarios/cadastrar', usuario)
 }
 
+logado(){
+  let ok: boolean = false
 
+  if(environment.token != ''){
+    ok = true
+  }
+
+  return ok
+}
 
 }
