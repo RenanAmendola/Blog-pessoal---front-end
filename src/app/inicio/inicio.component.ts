e8ac26f5939c0d83edc25433101cf90f5b5546e7
Pc Bia - Substituto <renanamendola@hotmail.com>
2022-06-08 20:53:04 -0300

    publicação funcionando

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { postagem } from '../model/postagem';
+import { tema } from '../model/tema';
+import { usuario } from '../model/usuario';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +14,19 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: postagem = new postagem()
+  
+  tema: tema = new tema()
+  listaTemas: tema[]
+  idTema: number
+
+  usuario: usuario = new usuario()
+  idUsuario = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit(){
@@ -19,6 +35,38 @@ export class InicioComponent implements OnInit {
         // alert('Sua seção expirou, faça o login novamente')
         this.router.navigate(['/entrar'])
       }
+
+      this.getAllTemas()
   }
+
+
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: tema[]) => {
+      this.listaTemas = resp
+    })
+
+  }
+ 
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: tema) => {
+      this.tema = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.usuario.id = this.idUsuario
+    this.postagem.usuario = this.usuario
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: postagem) => {
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new postagem()
+    })
+
+  }
+
 
 }
